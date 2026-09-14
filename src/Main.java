0fe25c5f54166ae68f6aa2e2e226169c38eaad97
pearl <24104062@usc.edu.ph>
2026-09-14 15:51:46 +0800

    version1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package version1;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("--- Hourly Employee Test ---");
+
+        HourlyEmployee hEmp1 = new HourlyEmployee(101, "Alice Smith");
+        hEmp1.setTotalHoursWorked(45.0f);
+        hEmp1.setRatePerHour(200.0);
+
+        HourlyEmployee hEmp2 = new HourlyEmployee(102, "Mark Cruz", 38.5f, 150.0);
+
+        System.out.println("\n[displayHourlyEmployee()]");
+        hEmp1.displayHourlyEmployee();
+        hEmp2.displayHourlyEmployee();
+
+        System.out.println("\n[toString()]");
+        System.out.println(hEmp1);
+        System.out.println(hEmp2);
+
+        System.out.println("\n--- Piece Worker Employee Test ---");
+
+        PieceWorkerEmployee pEmp1 = new PieceWorkerEmployee(201, "Bob Jones");
+        pEmp1.setTotalPiecesFinished(250);
+        pEmp1.setRatePerPiece(15.0);
+
+        PieceWorkerEmployee pEmp2 = new PieceWorkerEmployee(202, "Grace Lim", 95, 12.5);
+
+        System.out.println("\n[displayPieceWorkerEmployee()]");
+        pEmp1.displayPieceWorkerEmployee();
+        pEmp2.displayPieceWorkerEmployee();
+
+        System.out.println("\n[toString()]");
+        System.out.println(pEmp1);
+        System.out.println(pEmp2);
+
+        System.out.println("\n--- Commission Employee Test ---");
+
+        CommissionEmployee cEmp1 = new CommissionEmployee(301, "Carla Reyes");
+        cEmp1.setTotalSale(75000.0);
+
+        CommissionEmployee cEmp2 = new CommissionEmployee(302, "Dennis Uy", 620000.0);
+
+        System.out.println("\n[displayCommissionEmployee()]");
+        cEmp1.displayCommissionEmployee();
+        cEmp2.displayCommissionEmployee();
+
+        System.out.println("\n[toString()]");
+        System.out.println(cEmp1);
+        System.out.println(cEmp2);
+
+        System.out.println("\n--- Base Plus Commission Employee Test ---");
+
+        BasePlusCommissionEmployee bEmp1 = new BasePlusCommissionEmployee(401, "Ella Santos");
+        bEmp1.setTotalSale(120000.0);
+        bEmp1.setBaseSalary(10000.0);
+
+        BasePlusCommissionEmployee bEmp2 = new BasePlusCommissionEmployee(402, "Frank Tan", 45000.0, 8000.0);
+
+        System.out.println("\n[displayBasePlusCommissionEmployee()]");
+        bEmp1.displayBasePlusCommissionEmployee();
+        bEmp2.displayBasePlusCommissionEmployee();
+
+        System.out.println("\n[toString()]");
+        System.out.println(bEmp1);
+        System.out.println(bEmp2);
     }
 }
